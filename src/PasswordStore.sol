b36331c7b17f18540341234d8575780f624e70ed
worksofallwin <princeallwin199@gmail.com>
2024-02-10 12:09:07 +0530

    complete password store audit

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18; // q is this the correct compiler version
+pragma solidity 0.8.18;
 
 /*
  * @author not-so-secure-dev
@@ -11,6 +11,7 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    // @audit the s_password variable is not actually private! This is not a safe place to secure your password.
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +24,8 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+    // @audit any user can set a password.
+    // missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -32,6 +35,8 @@ contract PasswordStore {
      * @notice This allows only the owner to retrieve the password.
      * @param newPassword The new password to set.
      */
+    // @audit there is not newPassword parameter!
+    // @param line should be removed.
     function getPassword() external view returns (string memory) {
         if (msg.sender != s_owner) {
             revert PasswordStore__NotOwner();
